4ee368b641a45330195c39fde2844154859e9357
Galmes Francisco <franciscogalmes@gmail.com>
2020-10-26 17:51:29 -0300

    Initial commit

---
+++ src/visEffect.h
@@ -0,0 +1,32 @@
+
+/*
+
+  WS2812B CPU and memory efficient library
+
+  Date: 28.9.2016
+
+  Author: Martin Hubacek
+  	  	  http://www.martinhubacek.cz
+  	  	  @hubmartin
+
+  Licence: MIT License
+
+*/
+
+#ifndef VISEFFECT_H_
+#define VISEFFECT_H_
+
+#include <stdint.h>
+
+void visInit();
+void visHandle();
+void visRainbow();
+//uint32_t Wheel();
+//void visDots();
+//void visColor();
+void visHandle2();
+void visBarrido();
+
+
+/*void controlbotones();*/
+#endif /* VISEFFECT_H_ */
